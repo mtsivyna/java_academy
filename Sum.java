48e1413fc8b343885340a770d01741bda315d056
mtsivyna <max_3d@mail.ru>
2013-08-05 01:42:33 +0300

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -1,17 +1,14 @@
 public class Sum {
-    public static void main(String [] args) {
+        public static void main(String [] args) {
         int sum = 0;
-		for (int j = 0; j <= args.length; j++){
-			if (args.length<=j) {
-				System.out.println("You've entered: "+j+" element(s)");
-			} else {
-				String[] arr1 = args[j].split(" ");
-				for (int i = 0; i < arr1.length; i++) {
-					int a = Integer.parseInt(arr1[i]);
-					sum += a;
-				}
+        int j;
+		for (int j = 0; j < args.length; j++){
+			String[] arr1 = args[j].split(" ");
+			for (int i = 0; i < arr1.length; i++) {
+				int a = Integer.parseInt(arr1[i]);
+				sum += a;
 			}
 		}
-	        System.out.println("Sum: "+sum);
+	        System.out.println("You've entered: "+j+"elemnt(s)"+"\nSum = "+sum);
 	}
 }
