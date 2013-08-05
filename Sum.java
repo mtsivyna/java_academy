9ff61a4b1265b7ab4c2ee2465257dce2565ecf9b
mtsivyna <max_3d@mail.ru>
2013-08-05 23:24:25 +0300

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -10,18 +10,17 @@ public class Sum {
       Current method is used for parsing input string
       identification values, summation operation and display result */
      
-    public static void main(String [] args) {
-        int sum = 0;	// Result sum value
-	int j;		// Number of values
+	public static void main(String [] args) {
+		int sum = 0;	// Result sum value
         
-	    for (j = 0; j < args.length; j++) {
-		String[] element = args[j].split(" ");
+		for (String j : args) {
+			String[] element = j.split(" ");
 		
-		    for (int i = 0; i < element.length; i++) {
-			int a = Integer.parseInt(element[i]);
-			sum += a;
-		    }
-	    }
-	System.out.println("You've entered: " + j + " elemnt(s)" + "\nSum = " + sum);
-    }
+		    	for (String i : element) {
+				int a = Integer.parseInt(i);
+				sum += a;
+		    	}
+	    	}
+	System.out.println("Sum = " + sum);
+	}
 }
