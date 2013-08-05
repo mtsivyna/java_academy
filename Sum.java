d5d9c307ba4e7141d45a3bce2b8405f11b9c4a44
mtsivyna <max_3d@mail.ru>
2013-08-05 12:55:27 +0300

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -1,14 +1,24 @@
+/**
+* Sum.java
+* This class outputs the sum of input values
+* @version    1.3 08/05/13
+* @author    Maksym Tsivyna 
+*/
+
 public class Sum {
-        public static void main(String [] args) {
+	
+    public static void main(String [] args) {
         int sum = 0;
         int j;
-		for (int j = 0; j < args.length; j++){
-			String[] arr1 = args[j].split(" ");
-			for (int i = 0; i < arr1.length; i++) {
-				int a = Integer.parseInt(arr1[i]);
-				sum += a;
-			}
-		}
-	        System.out.println("You've entered: "+j+"elemnt(s)"+"\nSum = "+sum);
-	}
+        
+	    for (j = 0; j < args.length; j++) {
+		String[] arr1 = args[j].split(" ");
+		
+		    for (int i = 0; i < arr1.length; i++) {
+			int a = Integer.parseInt(arr1[i]);
+			sum += a;
+		    }
+	    }
+	        System.out.println("You've entered: "+j+" elemnt(s)"+"\nSum = "+sum);
+    }
 }
