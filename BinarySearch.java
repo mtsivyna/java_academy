5c416208408926796e59a0430e84c541567f4b8d
mtsivyna <max_3d@mail.ru>
2013-08-12 17:14:21 +0300

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -6,16 +6,16 @@
  */
 
 public class BinarySearch{
-	/**
-	Current method is used for parsing input string,
-	identification values, display search result */
+    /**
+    Current method is used for parsing input string,
+    identification values, display search result */
 	
-    public static void main(String[] args) {
+    public static void main(String[] args){
 		
         int[] array = new int[args.length-1];    // array length = args length - 1 
-		int numberOfElements = array.length;     // number of elements in array
+	    int numberOfElements = array.length;    // number of elements in array
 
-        for (int elementId = 1; elementId <= numberOfElements; elementId++) {
+        for (int elementId = 1; elementId <= numberOfElements; elementId++){
             array[elementId - 1] = Integer.parseInt(args[elementId]);
         }
         System.out.println("Inserted array :");
@@ -27,22 +27,22 @@ public class BinarySearch{
         int searchVal = Integer.parseInt(args[0]);    // value that should be found in array
 
         System.out.println("Recursive method: Element: " + searchVal + " is located on " +
-                recursiveBinarySearch(array, searchVal) + " position");
+            recursiveBinarySearch(array, searchVal) + " position");
         System.out.println("Iterative number: Element: " + searchVal + " is located on " +
-                iterativeBinarySearch(array, searchVal) + " position");
+            iterativeBinarySearch(array, searchVal) + " position");
     }
 	
-	/**
-	Current method is used for defining array element that equals search value
-	using iterative method	*/
+    /**
+    Current method is used for defining array element that equals search value
+    using iterative method */
 	
-    public static int iterativeBinarySearch (int[] array, int searchVal) {
+    public static int iterativeBinarySearch (int[] array, int searchVal){
 
-        int firstIndex = 0;    				 // Index of first element
+        int firstIndex = 0;    		     // Index of first element
         int lastIndex = array.length - 1;    // Index of last element
 
-        while (firstIndex <= lastIndex) {
-            int mid = firstIndex + (lastIndex - firstIndex) / 2;	// Index of mid value
+        while (firstIndex <= lastIndex){
+            int mid = firstIndex + (lastIndex - firstIndex) / 2;    // Index of mid value
 
             if (searchVal < array[mid])
                 lastIndex = --mid;
@@ -56,11 +56,11 @@ public class BinarySearch{
         return -1;
     }
 	
-	/**
-	Current methods are used for defining array element that equals search value 
-	using recursive search method*/
+    /**
+    Current methods are used for defining array element that equals search value 
+    using recursive search method */
 	
-    public static int recursiveBinarySearch(int[] array, int searchVal) {
+    public static int recursiveBinarySearch(int[] array, int searchVal){
 
         return recursiveBinarySearch (array, searchVal, 0, array.length - 1);
     }
@@ -68,7 +68,7 @@ public class BinarySearch{
     public static int recursiveBinarySearch (int[] array, int searchVal, int firstIndex, int lastIndex){
 
         while (firstIndex <= lastIndex){
-            int mid = firstIndex + (lastIndex - firstIndex) / 2;	// Index of mid value
+            int mid = firstIndex + (lastIndex - firstIndex) / 2;    // Index of mid value
 
             if(searchVal > array[mid])
                 return recursiveBinarySearch(array, searchVal, ++mid, lastIndex);
